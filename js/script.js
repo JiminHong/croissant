e9a7aa0af91117e4f1b4904f892993f7b1632230
JiminHong <jiminhong@jimins-mbp.fullsail.com>
2015-03-23 21:16:07 -0400

    sync

--- js/script.js
+++ js/script.js
@@ -6,4 +6,37 @@
     menuWidth: 240
   });
   
+  var headings = $("#accordion h2");
+var sections = $("section");
+sections.hide();
+
+
+//When a header is clicked, the first thing to do is check whether
+//the paragraph underneath it is visible. If it is, then you don’t
+//need to do anything.
+
+//next gets the element that immediately follows the current one in the DOM
+//Using the return keyword causes the function to stop execution at that point, and no further code in that function will be run.
+
+//Hiding the visible paragraph, in this instance it’s much easier to hide them all, and then show only the one you need.
+
+$(function() {
+var headings = $("#accordion h2");
+var sections = $("section");
+sections.not(":first").hide();
+headings.on("click", function() {
+var t = $(this);
+//Made var t into another variable because we used it more than once.
+var section = t.next();
+if(section.is(":visible")) {
+ section.hide();
+}
+else
+{
+sections.hide();
+section.show();
+}
+
+});
+});
 })(jQuery);
